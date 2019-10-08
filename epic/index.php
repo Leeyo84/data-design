ef174c34980fba9cd53574ec68a42331a5f99f1b
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-10-08 09:46:15 -0600

    added index.php, interaction-flow.html, persona.html,user-story.html, conceptual-model.html

--- epic/index.php
+++ epic/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Document</title>
+		<title>Data Design Project</title>
 	</head>
 	<body>
 		<link rel="stylesheet" href="interaction-flow.html">
