427ae7202b017f8baee19f821fcfd9473f11c898
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-10-08 13:01:38 -0600

    added style.css

--- epic/index.php
+++ epic/index.php
@@ -1,14 +1,18 @@
 <!doctype html>
 <html lang="en">
 	<head>
+		<link rel="stylesheet" href="style.css">
 		<meta charset="UTF-8">
 		<title>Data Design Project</title>
 	</head>
-	<body>
-		<link rel="stylesheet" href="interaction-flow.html">
-		<link rel="stylesheet" href="persona.html">
-		<link rel="stylesheet" href="user-story.html">
-		<link rel="stylesheet" href="conceptual-model.html">
 
+	<body>
+		<h1>Data Design Project</h1>
+		<ul>
+			<li> <a href="persona.html">Persona</a> </li>
+			<li> <a href="user-story.html">User Story</a> </li>
+			<li> <a href="interaction-flow.html">Interaction Flow</a> </li>
+			<li> <a href="conceptual-model.html">Conceptual Model</a> </li>
+		</ul>
 	</body>
 </html>
