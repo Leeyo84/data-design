23ab613aee70ca5d58697530957647198e7d6565
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-10-09 16:29:43 -0600

    made changes to links on all .html

--- epic/index.php
+++ epic/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Data Design Project</title
+		<title>Data Design Project</title>
 	</head>
 
 	<body>
@@ -11,12 +11,13 @@
 			<div class="inner"></div>
 			<h1>Data Design Project</h1>
 		</div>
+
 		<div>
 			<ul>
-				<li id="container"> <a href="persona.html">Persona</a> </li>
-				<li> <a href="user-story.html">User Story</a> </li>
-				<li> <a href="interaction-flow.html">Interaction Flow</a> </li>
-				<li> <a href="conceptual-model.html">Conceptual Model</a> </li>
+				<li id="menuLinks"> <a href="persona.html">Persona</a> </li>
+				<li id="menuLinks"> <a href="user-story.html">User Story</a> </li>
+				<li id="menuLinks"> <a href="interaction-flow.html">Interaction Flow</a> </li>
+				<li id="menuLinks"> <a href="conceptual-model.html">Conceptual Model</a> </li>
 			</ul>
 		</div>
 	</body>
