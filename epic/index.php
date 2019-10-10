bf19e3f754c6924bd3af31b67b146f4a1aac01fc
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-10-09 20:35:09 -0600

    worked on centering headings

--- epic/index.php
+++ epic/index.php
@@ -7,19 +7,16 @@
 
 	<body>
 	<link rel="stylesheet" href="style.css">
-		<div class="divider">
-			<div class="inner"></div>
-			<h1>Data Design Project</h1>
-		</div>
-
-		<div>
-			<ul>
-				<li class="menuLinks"> <a href="persona.html">Persona</a> </li>
-				<li class="menuLinks"> <a href="user-story.html">User Story</a> </li>
-				<li class="menuLinks"> <a href="interaction-flow.html">Interaction Flow</a> </li>
-				<li class="menuLinks"> <a href="conceptual-model.html">Conceptual Model</a> </li>
-			</ul>
-		</div>
+	<h1>Data Design Project</h1>
+	<div class="menu">
+		<ul>
+			<li class="menuLinks"> <a href="index.php">Home</a> </li>
+			<li class="menuLinks"> <a href="persona.html">Persona</a> </li>
+			<li class="menuLinks"> <a href="user-story.html">User Story</a> </li>
+			<li class="menuLinks"> <a href="interaction-flow.html">Interaction Flow</a> </li>
+			<li class="menuLinks"> <a href="conceptual-model.html">Conceptual Model</a> </li>
+		</ul>
+	</div>
 
 	</body>
 </html>
